a6d591f37635dda91de31f62423949453a81a154
Kasper Oikarinen <kasper@mrfriday.com>
2022-10-22 20:52:26 +0200

    Create BLASTDB + run BLAST

--- flams/main.py
+++ flams/main.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 
 from Bio import SeqIO
+from display import display_result
 from run_blast import run_blast
 
 
@@ -11,24 +12,43 @@ def read_fasta(file: Path):
 
 
 def main(args):
-    protein_seq = read_fasta(Path(args.input[0]))
-    run_blast(protein_seq)
-    # display_result(result)
+    protein_seq = read_fasta(Path(args.input))
+
+    result = run_blast(
+        protein_seq,
+        input=args.input,
+        lysine_pos=args.lysine_pos,
+        lysine_range=args.lysine_range,
+        num_threads=args.num_threads,
+    )
+    display_result(result)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Find Lysine Acetylation Modification Sites."
     )
-    parser.add_argument("input", nargs=1, type=str, help="Path to input .fasta file.")
+    parser.add_argument("input", type=str, help="Path to input .fasta file.")
     parser.add_argument(
-        "-o",
-        "--output",
-        nargs=1,
+        "lysine_pos", type=str, help="Position in input where to search for Acetylation"
+    )
+    parser.add_argument(
+        "lysine_range",
         type=str,
-        default="out.tsv",
-        help="Path to output .tsv file.",
+        help="Range in position where to search for Acetylation",
+    )
+
+    parser.add_argument(
+        "-o", "--output", type=str, default="out.tsv", help="Path to output .tsv file."
+    )
+
+    # BLAST settings
+    parser.add_argument(
+        "-t",
+        "--num_threads",
+        type=int,
+        default=1,
+        help="Number of threads to run BLAST with",
     )
     args = parser.parse_args()
-
     main(args)
