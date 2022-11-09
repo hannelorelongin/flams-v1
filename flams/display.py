a989c0b841c3304c1049a985def0142c59a0fbec
RoshanXXX <roshanpalakkad@gmail.com>
2022-11-09 16:52:15 +0100

    Isolated pubmedId and species name, along  with lysinewindow

--- flams/display.py
+++ flams/display.py
@@ -5,20 +5,29 @@ def display_result(blast_records):
     # http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec129
     with open(r"output.tsv", "w") as out_file:
         tsv_writer = csv.writer(out_file, delimiter="\t")
-        tsv_writer.writerow(["Uniprot ID", "Species", "lysine location"])
+        tsv_writer.writerow(
+            ["Uniprot ID", "Species", "lysine location", "Lysine Window"]
+        )
         for blast_record in blast_records:
             for alignment in blast_record.alignments:
                 for hsp in alignment.hsps:
-                    pubmedID = (alignment.title).split("|")
-                    Species_name = pubmedID[4].split("[")
-                    lysine = Species_name[0]
+                    pubmedID = (alignment.title).split("|")  # from title pubmed_Id
+                    Species = pubmedID[4].split("[")  # isolates species name
+                    lysine = Species[0]
                     lysine_no = lysine.split()
-                    Species_name = Species_name[1].split("]")
-                    lysine_location = int(lysine_no[0])
-                    # print(lysine_location)
-                    # print(alignment.title)
-                    # hit_seq= [x for x in hsp.sbjct]
-                    # mod_pos= lysine_location - (hsp.sbjct_start-1) +1
-                    # if lysine_location <= hsp.sbjct_end and lysine_location >= hsp.sbjct_start:
-                    #     print(hsp.sbjct[mod_pos-5:mod_pos+5])
-                    tsv_writer.writerow([pubmedID[3], Species_name, lysine_location])
+                    Species_name = Species[1].split("]")
+                    lysine_location = int(
+                        lysine_no[0]
+                    )  # gets lysine no from the title alignment
+                    no_dashes = hsp.sbjct.replace(
+                        "-", ""
+                    )  # dashes in alignment are replaced for getting lysine window
+                    mod_pos = lysine_location - (
+                        hsp.sbjct_start
+                    )  # lysine location calculated relative to blast file
+                    lysine_window = no_dashes[
+                        mod_pos - 5 : mod_pos + 6
+                    ]  # lysine window islaoted (Range set to 5)
+                    tsv_writer.writerow(
+                        [pubmedID[3], Species_name[0], lysine_location, lysine_window]
+                    )
