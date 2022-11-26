f3fb71e53277091a32d950525b7553e139989ff9
RoshanXXX <roshanpalakkad@gmail.com>
2022-11-26 19:47:03 +0100

    Final touches for output generation/added improvement to lysine window

--- flams/display.py
+++ flams/display.py
@@ -6,7 +6,7 @@ def display_result(output_filename, blast_records):
     with open(output_filename, "w") as out_file:
         tsv_writer = csv.writer(out_file, delimiter="\t")
         tsv_writer.writerow(
-            ["Uniprot ID", "Species", "lysine location", "Lysine Window"]
+            ["Uniprot ID", "Species", "Lysine location", "Lysine Window"]
         )
         for blast_record in blast_records:
             for alignment in blast_record.alignments:
@@ -28,6 +28,44 @@ def display_result(output_filename, blast_records):
                     lysine_window = no_dashes[
                         mod_pos - 5 : mod_pos + 6
                     ]  # lysine window islaoted (Range set to 5)
+                    start = str(mod_pos - 5 + hsp.sbjct_start)
+                    end = str(mod_pos + 6 + hsp.sbjct_start)
+                    window = start + "-" + lysine_window + "-" + end
                     tsv_writer.writerow(
-                        [pubmedID[1], Species_name[0], lysine_location, lysine_window]
+                        [pubmedID[1], Species_name[0], lysine_location, window]
+                    )
+
+
+def display_result2(output_filename, blast_records):
+    # http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec129
+    with open(output_filename, "w") as out_file:
+        tsv_writer = csv.writer(out_file, delimiter="\t")
+        tsv_writer.writerow(
+            ["Uniprot ID", "Species", "lysine location", "Lysine Window"]
+        )
+        for blast_record in blast_records:
+            for alignment in blast_record.alignments:
+                for hsp in alignment.hsps:
+                    pubmedID = (alignment.title).split("|")  # from title pubmed_Id
+                    Species = pubmedID[4].split("[")  # isolates species name
+                    lysine = Species[0]
+                    lysine_no = lysine.split()
+                    Species_name = Species[1].split("]")
+                    lysine_location = int(
+                        lysine_no[0]
+                    )  # gets lysine no from the title alignment
+                    no_dashes = hsp.sbjct.replace(
+                        "-", ""
+                    )  # dashes in alignment are replaced for getting lysine window
+                    mod_pos = lysine_location - (
+                        hsp.sbjct_start
+                    )  # lysine location calculated relative to blast file
+                    lysine_window = no_dashes[
+                        mod_pos - 5 : mod_pos + 6
+                    ]  # lysine window islaoted (Range set to 5)
+                    start = str(mod_pos - 5 + hsp.sbjct_start)
+                    end = str(mod_pos + 6 + hsp.sbjct_start)
+                    window = start + "-" + lysine_window + "-" + end
+                    tsv_writer.writerow(
+                        [pubmedID[3], Species_name[0], lysine_location, window]
                     )
--- test/test_display.py
+++ test/test_display.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import unittest
 
 from Bio.Blast import NCBIXML
-from flams.display import display_result
+from flams.display import display_result2
 
 
 EXAMPLE_BLAST_OUTPUT = Path(__file__).parent / "testfiles/acetylation.out"
@@ -12,7 +12,7 @@ class DisplayTestCase(unittest.TestCase):
     def test_display_simple(self):
         with EXAMPLE_BLAST_OUTPUT.open() as f:
             blast_records = list(NCBIXML.parse(f))
-            display_result(blast_records)
+            display_result2(output_filename="out.tsv", blast_records=blast_records)
 
 
 if __name__ == "__main__":
