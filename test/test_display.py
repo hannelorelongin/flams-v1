88ded2548b057e67ce24a6318026c0f7a1994bff
RoshanXXX <roshanpalakkad@gmail.com>
2022-11-27 19:38:17 +0100

    Improved unittest example file

--- test/test_display.py
+++ test/test_display.py
@@ -2,17 +2,17 @@ from pathlib import Path
 import unittest
 
 from Bio.Blast import NCBIXML
-from flams.display import display_result2
+from flams.display import display_result
 
 
-EXAMPLE_BLAST_OUTPUT = Path(__file__).parent / "testfiles/acetylation.out"
+EXAMPLE_BLAST_OUTPUT = Path(__file__).parent / "testfiles/blast_output_P57703.xml"
 
 
 class DisplayTestCase(unittest.TestCase):
     def test_display_simple(self):
         with EXAMPLE_BLAST_OUTPUT.open() as f:
             blast_records = list(NCBIXML.parse(f))
-            display_result2(output_filename="out.tsv", blast_records=blast_records)
+            display_result(output_filename="out.tsv", blast_records=blast_records)
 
 
 if __name__ == "__main__":
