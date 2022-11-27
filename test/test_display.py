18b9e7daafde68100db7cd107901f39e4c1e882e
RoshanXXX <roshanpalakkad@gmail.com>
2022-11-27 20:56:19 +0100

    Improved unittest(with assertions) example file

--- test/test_display.py
+++ test/test_display.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import unittest
+import csv
 
 from Bio.Blast import NCBIXML
 from flams.display import display_result
@@ -12,7 +13,17 @@ class DisplayTestCase(unittest.TestCase):
     def test_display_simple(self):
         with EXAMPLE_BLAST_OUTPUT.open() as f:
             blast_records = list(NCBIXML.parse(f))
-            display_result(output_filename="out.tsv", blast_records=blast_records)
+            display_result(output_filename="out_example.tsv", blast_records=blast_records)
+        self.assertTrue(Path("out_example.tsv").is_file())
+        out_example = Path("out_example.tsv")
+        with out_example.open() as t:
+            example_table = csv.reader(t, delimiter="\t")
+            row1 = next(example_table)
+            row2 = next(example_table)
+            self.assertEqual(row2[0],"P25665", "The Uniprot Id is not correct in the example output file")
+            self.assertEqual(int(row2[2]),87, "The lysine location is not correctly displayed")
+            self.assertEqual(row2[1],"Escherichia coli (strain K12)", "The name of the species is not displayed correctly")
+            self.assertEqual(row2[3],"82-ARHQNKDGSVD-93", "The lysine window is not correctly displayed")
 
 
 if __name__ == "__main__":
