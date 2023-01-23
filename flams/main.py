b1414e14b5fb4268528ff5990d02c621fcbacd18
Hannelore Longin <hannelore.longin@student.kuleuven.be>
2023-01-23 15:33:27 +0100

    verify blast+ installation

--- flams/main.py
+++ flams/main.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: kasgel, Retro212, annkamsk, hannelorelongin
+"""
+
 from pathlib import Path
 import sys
+import shutil
 
 from flams.display import display_result
 from flams.input import parse_args
@@ -9,8 +16,19 @@ from flams.databases.setup import update_db_for_modifications
 
 DATA_PATH = Path(__file__).parents[1] / "data"
 
+def is_available(program):
+    """Verify if third party dependency program is available on the PATH"""
+
+    if shutil.which(program) is not None:
+        print(program + " installed: OK")
+    else:
+        print(program + " installed: Not available on the path.. exiting FLAMS.")
+        sys.exit()
 
 def main(args, protein_file):
+
+    is_available('blastp')
+
     update_db_for_modifications(args.modification)
 
     # Save absolute path to output file, because run_blast will change working directory.
