e522dd2a4c13b56eccb81711cf72990e56199e03
Hannelore Longin <hannelore.longin@student.kuleuven.be>
2023-02-19 17:45:57 +0100

    add documentation

--- flams/utils.py
+++ flams/utils.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: kasgel
+"""
+
 from pathlib import Path
 import appdirs
 
+""" setup
+This script deals with getting a platform-specific directory to store app data.
+E.g. on     Linux: ~/.local/share/<AppName>
+            Mac: '/Users/trentm/Library/Application Support/SuperApp'
+            Windows: 'C:\\Users\\trentm\\AppData\\Local\\Acme\\SuperApp'
+"""
 
-# This will get a platform-specific dir to store app data.
-# E.g. on Linux: ~/.local/share/<AppName>
-# Mac: '/Users/trentm/Library/Application Support/SuperApp'
-# Windows: 'C:\\Users\\trentm\\AppData\\Local\\Acme\\SuperApp'
 def get_data_dir(app_name="flams"):
     # Ensure data dir exists and return.
     data_dir = appdirs.user_data_dir(app_name)
