121838c47996339df4942265b5b1e5f227899e5a
Hannelore Longin <hannelore.longin@student.kuleuven.be>
2023-02-19 17:39:51 +0100

    add documentation

--- flams/display.py
+++ flams/display.py
@@ -1,8 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Retro212, annkamsk
+"""
+
 import csv
 
+""" setup
+This script deals with returning the results of FLAMS to the user in a tsv file.
+"""
 
 def display_result(output_filename, blast_records):
-    # http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec129
+    """
+    This function creates a tsv file containing all conserved modification sites, based on a specific FLAMS run.
+
+    Parameters
+    ----------
+    output_filename: str
+        Output file name
+    blast_records: array
+        Array containing BLAST records that met search criteria of FLAMS run.
+
+    """
     with open(output_filename, "w") as out_file:
         tsv_writer = csv.writer(out_file, delimiter="\t")
         tsv_writer.writerow(
@@ -33,7 +52,7 @@ def display_result(output_filename, blast_records):
                     )  # lysine location calculated relative to blast file
                     lysine_window = no_dashes[
                         mod_pos - 5 : mod_pos + 6
-                    ]  # lysine window islaoted (Range set to 5)
+                    ]  # lysine window isolated (Range set to 5)
                     start = str(mod_pos - 5 + hsp.sbjct_start)
                     end = str(mod_pos + 6 + hsp.sbjct_start)
                     window = (
